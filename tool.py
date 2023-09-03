16a6b63dabb3d3805d5728f34411879f16bb8072
mohamed hack <128503815+mohamedmosamir@users.noreply.github.com>
2023-09-03 03:33:38 +0300

    Add files via upload
    The (Tool.py) tool is the best tool to help in installing tools or a few tools, including the Metaspolet, Root, and Amer tool, where the basics of (Termux) can also 
    
    -----------------------------------
    be installed to run the tool:
    
    cd tool
    
    python tool.py 
    ---------------------------------
    
    
    to run the tool: 
    
    python 
    
    python2
    
    python3 
    
    wget 
    
    git
    
    tool works very well and has no malfunctions at all

---
+++ tool.py
@@ -0,0 +1,418 @@
+
+
+import random
+import requests
+import socket
+import os
+
+os.system('clear')
+
+
+# Bold
+Bz="\033[1;30m" # Black
+Br="\033[1;31m" # Red
+Bg="\033[1;32m" # Green
+By="\033[1;33m" # Yellow
+Bn="\033[1;34m" # Blue
+Bp="\033[1;35m" # Purple
+Bc="\033[1;36m" # Cyan
+Bw="\033[1;37m" # White
+# Underline
+Ub="\033[4;30m" # Black
+UR="\033[4;31m" # Red
+UG="\033[4;32m" # Green
+UY="\033[4;33m" # Yellow
+UB="\033[4;34m" # Blue
+UPe="\033[4;35m" # Purple
+UC="\033[4;36m" # Cyan
+UW="\033[4;37m" # White
+# Background
+Ab="\033[40m" # Black
+AR="\033[41m" # Red
+AG="\033[42m" # Green
+AY="\033[43m" # Yellow
+AB="\033[44m" # 
+AP="\033[45m" # Purple
+AC="\033[46m" # Cyan
+AW="\033[47m" # White
+# High Intensty
+Ib="\033[0;90m" # Black
+IR="\033[0;91m" # Red
+IG="\033[0;92m" # Green
+IY="\033[0;93m" # Yellow
+IB="\033[0;94m" # Blue
+IP="\033[0;95m" # Purple
+IC="\033[0;96m" # Cyan
+IW="\033[0;97m" # White
+# Bold High Intensty
+Bb="\033[1;90m" # Black
+BR="\033[1;91m" # Red
+BG="\033[1;92m" # Green
+BY="\033[1;93m" # Yellow
+BB="\033[1;94m" # Blue
+BP="\033[1;95m" # Purple
+BC="\033[1;96m" # Cyan
+BW="\033[1;97m" # White
+# High Intensty backgrounds
+Ob="\033[0;100m" # Black
+OR="\033[0;101m" # Red
+OG="\033[0;102m" # Green
+OY="\033[0;103m" # Yellow
+OB="\033[0;104m" # Blue
+OP="\033[10;95m" # Purple
+OC="\033[0;106m" # Cyan
+OW="\033[0;107m" # White
+
+a = "\033[0;32m" # Green
+X="\033[0;30m" # Black
+R="\033[0;31m" # Red
+Y="\033[0;33m" # Yellow
+Z="\033[0;34m" # Blue
+P="\033[0;35m" # Purple
+C="\033[0;36m" # Cyan
+W="\033[0;37m" # White
+
+
+print(a+ '''  ______            __
+ /_  __/___  ____  / /
+  / / / __ / __ / / 
+ / / / /_/ / /_/ / /  
+/_/  ____/____/_/   
+                     ''')
+                                          
+def gethtxtool ():
+	os.system('clear')
+	os.system('git clone https://github.com/kuburan/txtool.git')
+	os.system('cd txtool')
+	os.system('apt install python2')
+	os.system('./install.py')
+	os.system('cd')
+	os.system('clear')
+	print('×'*60)
+	print(' ')
+	print('                         cd txtool ')
+	print(' ')
+	print('                            txtool')
+	print(' ')
+	print('           copy to paste to open Txtool ')
+	print(' ')
+	print('×'*60)
+	
+	
+def gethLazymux ():
+		os.system('clear')
+		os.system('pkg install python git')
+		os.system('git clone https://github.com/Gameye98/Lazymux')
+		os.system('cd Lazymux')
+		os.system('python lazymux.py')
+		os.system('cd')
+		os.system('clear')
+		print('✓'*60)
+		print(' ')
+		print('              cd Lazymux ')
+		print(' ')
+		print('              python lazymux.py')
+		print(' ')
+		print('              copy to paste to open Lazymux ')
+		print(' ')
+		print('✓'*60)
+		
+def gethngrok ():
+		os.system('clear')		
+		os.system('apt update')
+		os.system('apt upgrade')
+		os.system('pkg install wget')
+		os.system('pkg install git')
+		os.system('wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz')
+		os.system('clear')
+		os.system('tar zxvf ngrok-v3-stable-linux-arm64.tgz ')
+		os.system('rm ngrok-v3-stable-linux-arm64.tgz')
+		os.system('chomd +x ngrok')
+		os.system('clear')
+		os.system('cd')
+
+
+def gethamer ():
+	os.system('clear')
+	os.system('git clone https://github.com/amerlaceset/Amer')
+	os.system('cd Amer')
+	os.system(' ./Amer.sh')
+	os.system('chmod +x Amer.sh')
+	os.system('cd')
+	os.system('clear')
+	print('✓'*60)
+	print(' ')
+	print('                                cd Amer')
+	print(' ')
+	print('                             bash Amer.sh')
+	print(' ')
+	print('                   copy to paste to open Amer ')
+	print(' ')
+	print('✓'*60)
+
+def gethnmap():
+	os.system('clear')
+	os.system('apt update')
+	os.system('apt upgrade')
+	os.system('pkg install nmap')
+	os.system('clear')
+
+		
+def gethemail():
+	os.system('clear')
+	os.system('git clone https://github.com/mohinparamasivam/Email_Bomber.git')
+	os.system('clear')
+	print('✓'*60)
+	print(" ")
+	print("                      cd Email.Bomber")
+	print(' ')
+	print("                      python emailbomber.py")
+	print(" ")
+	print('                                     copy to paste to open Email.Bomber')
+	print(' ')
+	print('✓'*60)
+	
+	
+def gethcmseek():
+	os.system('git clone https://github.com/Tuhinshubhra/CMSeeK')
+	os.system('cd CMSeeK')
+	os.system('chomd +x*')
+	os.system('cd')
+	os.system('clear')
+	print('✓'*60)
+	print(" ")
+	print("                  cd CMSeek")
+	print(' ')
+	print("                  python CMSeek.py")
+	print(" ")
+	print('                    copy to paste to open CMSeek')
+	print(' ')
+	print('✓'*60)
+
+def gethshellphish():
+	os.system('apt update && apt upgrade -y && apt install git wget php unzip curl -y && git clone https://github.com/AbirHasan2005/ShellPhish && cd ShellPhish && chmod +x *')
+	os.system('cd')
+	os.system('clear')
+	print('✓'*60)
+	print(' ')
+	print('                            cd ShellPhish')
+	print(' ')
+	print('                          bash shellphish.sh')
+	print(' ')
+	print('                      Copy To Paste To Open ShellPish')
+	print('✓'*60)
+
+def gethcamphish():
+	os.system('clear')
+	os.system('git clone https://github.com/techchipnet/CamPhish')
+	os.system('clear')
+	print('✓'*60)
+	print(' ')
+	print('                            cd CamPhish')
+	print(' ')
+	print('                              bash camphish.sh')
+	print(' ')
+	print('                                Copy To Paste To Open Camphish')
+	print('✓'*60)
+	
+	
+	
+def gethmetasploit():	
+	os.system('clear')
+	os.system('pkg install unstable-repo')
+	os.system('pkg install metasploit')
+	os.system('clear')
+	print('✓'*60)
+	print(' ')
+	print('msfconsole')
+	print(' ')
+	print('OR')
+	print(' ')
+	print('./msfconsole')
+	print(' ')
+	print('✓'*60)
+	
+def gethcaredmap():	
+	os.system('clear')
+	os.system('git clone https://github.com/lightos/credmap')
+	os.system('cd credmap')
+	os.system('chmod +x *')
+	os.system('cd')
+	os.system('clear')
+	print('✓'*60)
+	print(' ')
+	print('cd credmap')
+	print(' ')
+	print('python2 credmap.py -h [halp]')
+	print(' ')
+	print('$ python2 credmap.py --username (name email) --email {enter email }@email.com')
+	print(' ')
+	print('✓'*60)
+
+def gethlucifer():
+    os.system('clear')
+    os.system('git clone https://github.com/rixon-cochi/Lucifer.git')
+    os.system('ls')
+    os.system('cd Lucifer')
+    os.system('chmod +x *')
+    os.system('bash setup.sh')
+    os.system('ls')
+    os.system('cd')
+    os.system('clear')
+    print('✓'*60)
+    print(' ')
+    print('cd Lucifer')
+    print(' ')
+    print('bash instacracker.sh')
+    print(' ')
+    print('Copy To Paste To Open Lucifer')
+    print('✓'*60)
+
+def gethroot():
+	os.system('clear')
+	os.system('apt update')
+	os.system('apt upgrade')
+	os.system('pkg install proot-distro')
+	os.system('proot-distro install ubuntu')
+	os.system('pkg install x11-repo')
+	os.system('proot-distro login ubuntu')
+	os.system('exit')
+	print('✓'*60)
+	print(' ')
+	print('Enter {proot-distro login ubuntu} ')
+	print(' ')
+	print('✓'*60)
+
+def termuxfish():
+	os.system('clear')
+	os.system('git clone https://github.com/msn-05/termux-fish.git')
+	os.system('cd termux-fish && chmod +x script.sh')
+	os.system('cd')
+	print('✓'*60)
+	print(' ')
+	print('cd termux-fish')
+	print(' ')
+	print('./script.sh')
+	print(' ')
+	print('Cooy To Paste To Open Termux-fish')
+	print('✓'*60)
+	
+def gethmdrok():
+	os.system('clear')
+	os.system('git clone https://github.com/Ranginang67/M-dork')
+	os.system('cd M-dork')
+	os.system('cd')
+	os.system('clear')
+	print('✓'*60)
+	print(' ')
+	print('cd M-dork')
+	print(' ')
+	print('python2 mdork.py')
+	print(' ')
+	print('Copy To Paste To Open M-dork')
+	print('✓'*60)
+
+def gethabout():
+	os.system('clear')
+	print('✓'*60)
+	print(' ')
+	print('         By : Mohamed Samir     &        Mohamed Ahmed ')
+	print(' ')
+	print('          youtube : https://youtube.com/@Nosh_afionaa')
+	print(' ')
+	print('          Telegram : https://t.me/Nosh_afionaa')
+	print(' ')
+	print('✓'*60)
+def gethexit():
+	os.system('clear')
+	os.system('cd')
+	os.system('clear')
+
+
+	 
+print(By + '[01] Txtool ')
+print(Bn + '[02] Lazymux ')
+print(Br + '[03] Ngrok ')    
+print(Bp + '[04] Amer ')
+print(Bg + '[05] Nmap ')
+print(IC + '[06] Email Bomder ')
+print(P + '[07] Cm Seek ')
+print(IB + '[08] Shellphish ')
+print(R + '[09] Camphish ')
+print(Bc + '[10] Metasploit ')
+print( UPe + '[11] Caredmap ')
+print( BY + '[12] Lucifer ')
+print( C + '[13] Root ')
+print( BB + '[14] Termux-fish ')
+print( Bw + '[15] M-dork ')
+print( IP + '[16] About  ')
+print( BY + '[00] Exit ')
+
+
+
+
+
+
+algeninput = input(R + "  == Select Nampr >>>   ")
+if algeninput == '1':
+	gethtxtool ()
+	
+elif algeninput == '2':
+	gethLazymux ()
+	
+elif algeninput == '3':
+	gethngrok ()
+    
+
+elif algeninput == '4':
+	gethamer ()
+    
+elif algeninput == '5':
+	gethnmap ()
+   
+elif algeninput == '6':
+	gethemail ()
+  
+elif algeninput == '7':
+	gethcmseek ()
+
+elif algeninput == '8':
+	gethshellphish ()
+
+elif algeninput == '9':
+	gethcamphish ()
+
+elif algeninput == '10':
+	gethcaredmap ()
+
+elif algeninput == '11':
+	gethmetasploit ()
+
+elif algeninput == '12':
+	gethlucifer ()
+
+elif algeninput == '13':
+	gethroot ()
+
+elif algeninput == '14':
+	gethtermuxfish ()
+
+elif algeninput == '15':
+	gethmdrok ()
+
+elif algeninput == '16':
+	gethabout ()
+
+elif algeninput == '0':
+	gethexit ()
+	
+else:
+    os.system('clear')
+    logoerror = '''
+███████╗██████╗░██████╗░░█████╗░██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+█████╗░░██████╔╝██████╔╝██║░░██║██████╔╝
+██╔══╝░░██╔══██╗██╔══██╗██║░░██║██╔══██╗
+███████╗██║░░██║██║░░██║╚█████╔╝██║░░██║
+╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝   '''
+    print( a + logoerror)
